cb9ef7f69c082965a667c1f837e5117a3aec4bcd
Arjun Divraniya <arjun.divraniya.cg@email.com>
2025-02-05 23:10:59 +0530

    Add user profile UI with navigation to subpages

--- Frontend/Shutter_sphere/src/App.jsx
+++ Frontend/Shutter_sphere/src/App.jsx
@@ -8,7 +8,12 @@ import Editprofile from "./Components/photographerprofile"
 import Login from "../src/Components/login";
 import { BrowserRouter as Router, Routes, Route ,useNavigate } from "react-router-dom";
 import Image from "./Components/claudinary"
-
+import Profile_p from "./Components/profile_p";
+import Profile_pay from "./Components/profile_pay";
+import Profile_r from "./Components/profile_r";
+import Profile_w from "./Components/profile_w";
+import Profile_s from "./Components/profile_s";
+import Profile_b from "./Components/profile_b";
 
 
 
@@ -32,6 +37,12 @@ function App() {
                 <Route path="/pgresult" element={<SearchResults />} />
                 <Route path="/profile" element={<Profile/>} />
                 <Route path="/editprofile" element={<Editprofile/>}/>
+                <Route path="/profile_profile" element={<><Profile/> <Profile_p/></>}/>
+                <Route path="/profile_booking" element={<><Profile/> <Profile_b/></>}/>
+                <Route path="/profile_payment" element={<><Profile/> <Profile_pay/></>}/>
+                <Route path="/profile_reviews" element={<><Profile/> <Profile_r/></>}/>
+                <Route path="/profile_settings" element={<><Profile/> <Profile_s/></>}/>
+                <Route path="/profile_Whishlist" element={<><Profile/> <Profile_w/></>}/>
 
             </Routes>
         </Router> 
